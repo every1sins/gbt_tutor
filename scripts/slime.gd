19fca2b7c40861f0b5636fb019f40620e315dfb0
“[Damien <[Kurtstone1999@gmail.com]>
2026-06-11 02:08:53 -0600

    enemy > animation > turn > player death

---
+++ scripts/slime.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = 1
+
+@onready var ray_castright: RayCast2D = $RayCastright
+@onready var ray_castleft: RayCast2D = $RayCastleft
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(_delta):
+	if ray_castright.is_colliding():
+		direction = -1 
+	if ray_castleft.is_colliding():
+		direction = 1
+	$AnimatedSprite2D.flip_h = direction < 0
+	position.x += direction *  SPEED * _delta
